4011aec382392ff7318f611d4c3966c383cde3f2
JeremyPerezSwift <jeremyperez34000@gmail.com>
2020-07-31 15:08:35 +0200

    Initial Commit

---
+++ ARPortal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARPortal
+//
+//  Created by Jérémy Perez on 31/07/2020.
+//  Copyright © 2020 Jérémy Perez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
